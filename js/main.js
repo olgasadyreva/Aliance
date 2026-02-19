eecb7c6640782bec2ae9daf5832b145d1366f236
olgasadyreva <olga.sadyreva@gmail.com>
2026-02-19 11:15:25 +0500

    fix: main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-const navbar = document.querySelector('.navbar');
-const logl = document.querySelector('.logo-svg use');
-window.addEventListener('scroll', () => {
+const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo-svg use");
+window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
-    navbar.classList.add('navbar-light');
-		logo.href.baseVal = 'img/sprite.svg#logo';
+    navbar.classList.add("navbar-light");
+    logo.href.baseVal = "img/sprite.svg#logo";
   } else {
-    navbar.classList.remove('navbar-light');
-		logo.href.baseVal = 'img/sprite.svg#logo-svg';
+    navbar.classList.remove("navbar-light");
+    logo.href.baseVal = "img/sprite.svg#logo-light";
   }
 });
