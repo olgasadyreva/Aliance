178897d67987f84c6a406cfe2f06ee0bbdab2ba2
olgasadyreva <olga.sadyreva@gmail.com>
2026-02-19 10:11:28 +0500

    fix: validate

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 const navbar = document.querySelector('.navbar');
+
 window.addEventListener('scroll', () => {
-	if (this.scrollY > 1) {
-		navbar.classList.add('navbar-light');
-	} else {
-		navbar.classList.remove('navbar-light');
-	}
-})
+  if (this.scrollY > 1) {
+    navbar.classList.add('navbar-light');
+  } else {
+    navbar.classList.remove('navbar-light');
+  }
+});
