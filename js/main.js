7c3c49c42ed18d6d94beada402881f10848f89b8
olgasadyreva <olga.sadyreva@gmail.com>
2026-03-03 20:17:19 +0500

    add: sliders

--- js/main.js
+++ js/main.js
@@ -35,3 +35,63 @@ mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
   menu.classList.contains("is-open") ? closeMenu() : openMenu();
 });
+
+const swiper = new Swiper(".swiper", {
+  speed: 400,
+  autoHeight: false,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1.6,
+    },
+    475: {
+      slidesPerView: 1.9,
+    },
+    576: {
+      slidesPerView: 2.4,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+const swiperSteps = new Swiper(".swiper.swiper-steps", {
+  speed: 400,
+  autoHeight: false,
+  slidesPerView: 1.5,
+  navigation: {
+    nextEl: ".swiper-steps .slider-button-next",
+    prevEl: ".swiper-steps .slider-button-prev",
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 1.3,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 2.5,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
