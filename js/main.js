8fdb8b42e11497e5c19c67f49c6bc3b189e99164
olgasadyreva <olga.sadyreva@gmail.com>
2026-04-03 09:40:25 +0500

    add: about-page and 3 variants of headers

--- js/main.js
+++ js/main.js
@@ -3,18 +3,19 @@ const logoLight = document.querySelector(".logo-light");
 const logo = document.querySelector(".logo");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
+const isFront = document.body.classList.contains('front-page');
 
 const lightModeOn = (event) => {
   navbar.classList.add("navbar-light");
-  logo.style.display = "block";
-  logoLight.style.display = "none";
 };
 
 const lightModeOff = (event) => {
   navbar.classList.remove("navbar-light");
-  logo.style.display = "none";
-  logoLight.style.display = "block";
 };
+
+const changeNavHeight = (height) => {
+	navbar.style.height = height;
+}
 
 const openMenu = (event) => {
   menu.classList.add("is-open");
@@ -31,7 +32,10 @@ const closeMenu = (event) => {
 };
 
 window.addEventListener("scroll", () => {
-  this.scrollY > 1 ? lightModeOn() : lightModeOff();
+  this.scrollY > 1 ? changeNavHeight('48px') : changeNavHeight('68px');
+	if(isFront) {
+		this.scrollY > 1 ? lightModeOn() : lightModeOff();
+	}
 });
 
 mMenuToggle.addEventListener("click", (event) => {
