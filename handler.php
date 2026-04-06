369613a352831879f30d221660295b9c157e5da3
olgasadyreva <olga.sadyreva@gmail.com>
2026-04-06 14:20:55 +0500

    add: telegram bot

--- handler.php
+++ handler.php
@@ -1,5 +1,25 @@
 <?php
-$user_name = $_POST["username"];
-$user_phone = $_POST["userphone"];
-echo "Привет, " . $user_name . "<br>";
-echo "Ваш телефон: <b>" . $user_phone . "<b>";
+$user_name = htmlspecialchars($_POST["username"]);
+$user_phone = htmlspecialchars($_POST["userphone"]);
+
+$token = "8743237870:AAGEuOhj-260Z4CUY2yfKOF2g58okhFz7l0";
+$chat_id = "-5174752769";
+
+$formData = array(
+  "Клиент: " => $user_name,
+  "Телефон: " => $user_phone
+);
+
+$text = "";
+
+foreach ($formData as $key => $value) {
+  $text .= $key . urlencode($value);
+}
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}", "r");
+
+if ($sendToTelegram) {
+  echo "Success";
+} else {
+  echo "Error";
+}
