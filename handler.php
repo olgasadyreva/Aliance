0b5c0f1f2149d627f0a199a2689800847180ab36
olgasadyreva <olga.sadyreva@gmail.com>
2026-04-06 15:06:55 +0500

    fix: handler.php

--- handler.php
+++ handler.php
@@ -39,10 +39,10 @@ $formData = array(
 $text = "";
 
 foreach ($formData as $key => $value) {
-  $text .= $key . "<b>" . $value . "</b>" . "%0A";
+  $text .= $key . "<b>" . urlencode($value) . "</b>" . "%0A";
 }
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}", "r");
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}&parse_mode=html", "r");
 
 if ($sendToTelegram) {
   echo "Success";
